dafac92edcf56eb6e9f06c9beab6c172529cc542
Dany <sstudanyst@gmail.com>
2022-05-23 16:06:13 +0400

    4

--- config.py
+++ config.py
@@ -1,9 +1,4 @@
 BOT_TOKEN = "5307046869:AAFJATWhlKfT9OUYe2hoc-Yt_wuytxAibJo"
 APP_URL = "https://creditcalculate-bot.herokuapp.com/" + BOT_TOKEN
-DB_URL = "postgres://mdtqgshedrdyps:ff8c32f87286242eb6284ffe57dab4c4298a0bd159e0f09c37831985af187694@ec2-52-30-67-143.eu-west-1.compute.amazonaws.com:5432/de0djp0nteftl6"
+DB_URI = "postgres://mdtqgshedrdyps:ff8c32f87286242eb6284ffe57dab4c4298a0bd159e0f09c37831985af187694@ec2-52-30-67-143.eu-west-1.compute.amazonaws.com:5432/de0djp0nteftl6"
 
-host = "ec2-52-30-67-143.eu-west-1.compute.amazonaws.com"
-user = "mdtqgshedrdyps"
-password = "ff8c32f87286242eb6284ffe57dab4c4298a0bd159e0f09c37831985af187694"
-db_name = "de0djp0nteftl6"
-port = 5432
